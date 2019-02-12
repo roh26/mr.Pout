ed8596df940da34a4576e31854769bc0aa1d6ec5
roh26 <47524378+roh26@users.noreply.github.com>
2019-02-12 10:13:09 +0530

    Create Ar.java

---
+++ Ar.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Ar
+
+{
+
+	public static void main(String arg[])
+	
+{
+	Scanner in = new Scanner(System.in);
+	
+	System.out.println("Enter the first number");
+	
+	int a = in.nextInt();
+
+	System.out.println("Enter the Second number");
+	
+	int b = in.nextInt();	
+	
+	int c =a+b;
+	
+	System.out.println("Result = " + c);
+
+}
+
+}
